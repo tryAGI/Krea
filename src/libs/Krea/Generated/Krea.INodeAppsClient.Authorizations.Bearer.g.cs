e4418110dc88cfe4ad1ee700dedc859ee22fb300
Konstantin S. <havendv@gmail.com>
2026-09-13 21:56:50 +0400

    feat: Updated OpenAPI spec (#29)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Krea/Generated/Krea.INodeAppsClient.Authorizations.Bearer.g.cs
@@ -0,0 +1,16 @@
+
+#nullable enable
+
+namespace Krea
+{
+    public partial interface INodeAppsClient
+    {
+        /// <summary>
+        /// Authorize using bearer authentication.
+        /// </summary>
+        /// <param name="apiKey"></param>
+
+        public void AuthorizeUsingBearer(
+            string apiKey);
+    }
+}
